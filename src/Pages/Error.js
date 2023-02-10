bfd76319ad06306e16b31e08fd9556b000117ef0
hardik3rddigital <hardik.s@3rddigital.com>
2023-02-10 14:18:07 +0530

    Error Page

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Error = (props) => {
+    const goback = useHistory();
+
     return(
         <>
             <div className="Home py-5">
                 <div className="container">
                     <div className="text-center">
                         <h1 className="text-white">Page Not Fount</h1>
+                        <p className="text-center text-white">Sorry, this page is not found</p>
+                        <button className="btn btn-light" onClick={()=> goback.goBack()}>Go to Home</button>
                     </div>
                 </div>
             </div>
